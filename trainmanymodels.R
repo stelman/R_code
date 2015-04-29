0ed74566371b577c3149ceee95f4cd1b6b82aeab
stelman <stylianos.kampakis@gmail.com>
2015-04-29 11:07:14 +0100

    function for training many different models in caret and collecting the results

---
+++ trainmanymodels.R
@@ -0,0 +1,21 @@
+library(caret)
+
+runManyModels<-function(formula,d,models=c("rf","gbm","svmRadial","svmPoly","nb","mlp","lm","leapSeq")){
+
+results=list()
+
+for(model in models){
+  if(typeof(d[1,all.vars(f)[1]])=="double" && model=="nb"){
+    break
+  }
+  print(model)
+  res=list()
+  m=train(formula,data=d,method=model)
+  res$error=min(m$results[[m$metric]])
+  res$bestTune=m$bestTune
+  results[length(results)+1]= list(res)
+}
+
+return(results)
+
+}
